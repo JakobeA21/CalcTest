a5dd1735760fcba2b18cc8bf0f2f4ea20d55d3eb
Jakobe <jakobe.alcorn@gmail.com>
2022-11-30 10:52:26 -0600

    First commit

---
+++ src/calculatormain/CalculatorMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculatormain;
+
+/**
+ *
+ * @author jakob
+ */
+public class CalculatorMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        calc g = new calc();
+        g.setSize(500, 525);
+        g.setVisible(true);
+    }
+    
+}
